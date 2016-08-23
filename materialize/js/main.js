e3794fc16f4f869252cdafaa351c41d2c214d55c
Jose Luis Rodriguez <burngeek8@gmail.com>
2016-08-23 00:11:22 -0500

    Llenar id del cliente en hidden

--- materialize/js/main.js
+++ materialize/js/main.js
@@ -4,11 +4,34 @@ $(document).ready(function(){
         $('.message').css({'display': 'none'});
         $('.progress').css({'display': 'block'})
     });
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
     $('.modal-trigger').leanModal();
     $('.btn-edit').on('click', editar);
+    $('#cliente').focusout(buscar_cliente);
     clientes();    
     ultimo_visto();
+    prestamo(15000, 4, 12, '03/15/2016');
 });
+function buscar_cliente(){
+    setTimeout(function(){ 
+        var input_cliente = $('#cliente').val()
+            url_base = "http://localhost:8080/primocash-api",
+            controlador = 'clientes',
+            metodo = 'nombre',
+            parametro = input_cliente.replace(/\s/g,"_");
+            url_consulta = url_base + '/' + controlador + '/' + metodo + '/' + parametro;
+        console.log(url_consulta);
+        $.getJSON(url_consulta, function(data){
+            console.log(data);
+            $('#customer_id').val(data.customer_id);
+            $('#pre_customer_name').html(data.customer_name);
+            $('#pre_customer_phone').html(data.customer_phone);
+        });
+     }, 500);
+}
 function editar(){
     var ultimo = $(this).parent('div').parent('li').attr('id');
     console.log(ultimo);
@@ -43,7 +66,8 @@ function permiso_menu(e, id_menu, id_user)
 }
 function clientes(){
     console.log('Clientes fue ejecutado');
-    $.getJSON("http://api.primocash.us/", function(data){
+    //$.getJSON("http://api.primocash.us/", function(data){
+    $.getJSON("http://localhost:8080/primocash-api/", function(data){
         //console.log(data);
         $('input.autocomplete.clientes').autocomplete(data);               
     });
@@ -64,8 +88,16 @@ function prestamo(capital, porcentaje, meses, fecha_inicio){
                 var day     = fecha.getDate();
                 var year    = fecha.getFullYear();
                 if(i != (meses - 1)){
+                    var html = `<p>Cuota Nro ${i+1}</p>
+                                <p>Monto: ${interes_mensual}.00 $USD</p> 
+                                <p>Fecha de pago: ${day}/${month}/${year}</p> 
+                                <br/>   `;
+                    $('#cuotas').append(html);
                     console.log('mes: ' + (i+1) + '\n Cuota a pagar: '+ interes_mensual +'.00 $USD\n fecha de pago: ' + day + '/' + month + '/' + year +'\n\n');
                 }else{
+                    var html = `<p>Cuota nro: ${i+1}</p>
+                                <p>Monto: ${ultima_cuota}.00 $USD</p>    `;
+                    $('#cuotas').append(html);
                     console.log('mes: ' + (i+1) + '\n Cuota a pagar: '+ ultima_cuota +'.00 $USD \n fecha de pago: ' + day + '/' + month + '/' + year +'\n\n');
                 }
             }
